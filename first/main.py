c2cc024e949509e146117e00b97d2a80db4e32e2
hovara <stoicafabianandrei@gmail.com>
2026-02-27 03:18:48 +0200

    Added fishing system; Added player head directional movement; Improve code readibility;

--- first/main.py
+++ first/main.py
@@ -2,6 +2,7 @@ import pyray as rl
 import raylib as rlc
 import math
 import random
+from enum import Enum
 
 
 class Vec2:
@@ -44,26 +45,33 @@ class Vec2:
 
 
 WINDOW_WIDTH = WINDOW_HEIGHT = 1000
-
-SAND = 1
-WATER = 2
-TREE = 3
-FIRE = 4
-FISH_LURE = 5
 BSIZE = 10
 
-AXE = 1
-FISHING_ROD = 2
 
-MOVING = 1
-FISHING = 2
+class Block(Enum):
+    SAND = 1
+    WATER = 2
+    TREE = 3
+    FIRE = 4
+    FISH_LURE = 5
+
+
+class Tool(Enum):
+    AXE = 1
+    FISHING_ROD = 2
+
+
+class State(Enum):
+    MOVING = 1
+    FISHING = 2
 
 
 class World:
     def __init__(self):
         self.size = Vec2(100, 100)
         self.blocks = [
-            [WATER for _ in range(int(self.size.x))] for _ in range(int(self.size.y))
+            [Block.WATER for _ in range(int(self.size.x))]
+            for _ in range(int(self.size.y))
         ]
         self.island_size = self.size.x * 0.8
 
@@ -73,29 +81,33 @@ class World:
 
         for y in range(start_pos, end_pos):
             for x in range(start_pos, end_pos):
-                self.blocks[y][x] = SAND
+                self.blocks[y][x] = Block.SAND
 
     def spawn_trees(self):
         for y in range(int(self.size.y)):
             for x in range(int(self.size.x)):
-                if self.blocks[y][x] != SAND:
+                if self.blocks[y][x] != Block.SAND:
                     continue
-                self.blocks[y][x] = TREE if random.uniform(0, 1) < 0.03 else SAND
+                self.blocks[y][x] = (
+                    Block.TREE if random.uniform(0, 1) < 0.03 else Block.SAND
+                )
 
     def draw(self):
         for y in range(int(self.size.y)):
             for x in range(int(self.size.x)):
-                if self.blocks[y][x] == SAND:
+                if self.blocks[y][x] == Block.SAND:
                     rlc.DrawRectangle(x * BSIZE, y * BSIZE, BSIZE, BSIZE, rlc.YELLOW)
-                elif self.blocks[y][x] == WATER:
+
+                elif self.blocks[y][x] == Block.WATER:
                     rlc.DrawRectangle(x * BSIZE, y * BSIZE, BSIZE, BSIZE, rlc.BLUE)
-                elif self.blocks[y][x] == TREE:
+
+                elif self.blocks[y][x] == Block.TREE:
                     # sand
                     rlc.DrawRectangle(x * BSIZE, y * BSIZE, BSIZE, BSIZE, rlc.YELLOW)
                     # trunk
                     rlc.DrawRectangleV(
-                        Vec2((x * BSIZE + BSIZE / 2 - BSIZE / 16), y * BSIZE)(),
-                        Vec2(int(BSIZE / 8), BSIZE)(),
+                        Vec2((x * BSIZE + BSIZE / 2 - BSIZE / 16), y * BSIZE + 1)(),
+                        Vec2(int(BSIZE / 8), BSIZE - 1)(),
                         rlc.BROWN,
                     )
                     # leaves
@@ -104,9 +116,11 @@ class World:
                         BSIZE * 0.3,
                         rlc.GREEN,
                     )
-                elif self.blocks[y][x] == FIRE:
+
+                elif self.blocks[y][x] == Block.FIRE:
                     rlc.DrawRectangle(x * BSIZE, y * BSIZE, BSIZE, BSIZE, rlc.ORANGE)
-                elif self.blocks[y][x] == FISH_LURE:
+
+                elif self.blocks[y][x] == Block.FISH_LURE:
                     rlc.DrawRectangle(x * BSIZE, y * BSIZE, BSIZE, BSIZE, rlc.BLUE)
                     rlc.DrawCircleV(
                         Vec2(x * BSIZE + BSIZE / 2, y * BSIZE + BSIZE / 2)(),
@@ -115,23 +129,113 @@ class World:
                     )
 
 
+class FishingManager:
+    TIME_LIMIT = 3
+    BASE_ANGULAR_SPEED = 300
+
+    def __init__(self):
+        self.timer = 0.0
+        self.lure_pos = Vec2(0, 0)
+        self.fish = 0
+
+        # do i need to put these here???
+        self.target_segment = 1
+        self.start_target = 20
+        self.end_target = 40
+
+        self.curr_speed = self.BASE_ANGULAR_SPEED
+        self.curr_angle = 0
+        self.dir = 1
+
+    def set_target(self):
+        self.target_segment = int(max(0, round(random.uniform(0, 1) * 10) - 2))
+        self.start_target = self.target_segment * 20
+        self.end_target = self.start_target + 20
+
+    def pull_fish(self):
+        if self.curr_angle >= self.start_target and self.curr_angle <= self.end_target:
+            self.fish += 1
+        self.timer = self.TIME_LIMIT
+
+    def update(self, player, world):
+        self.timer += rlc.GetFrameTime()
+
+        self.curr_angle += self.dir * self.curr_speed * rlc.GetFrameTime()
+        if self.curr_angle <= 0 or self.curr_angle >= 180:
+            self.dir *= -1
+            self.curr_speed += self.curr_speed * 0.2
+
+        if self.timer >= self.TIME_LIMIT:
+            self.timer = 0
+            player.state = State.MOVING
+            world.blocks[int(self.lure_pos.y)][int(self.lure_pos.x)] = Block.WATER
+            self.lure_pos = Vec2(0, 0)
+            self.curr_speed = self.BASE_ANGULAR_SPEED
+
+    def draw(self):
+        # indicator bg
+        outer_radius = WINDOW_WIDTH / 2 - 100
+        center = Vec2(WINDOW_WIDTH // 2, WINDOW_HEIGHT - outer_radius)
+        seg_angle = 20
+        segments = 9
+
+        rlc.DrawRectangle(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, rlc.Fade(rlc.BLACK, 0.5))
+
+        for s in range(segments):
+            color = rlc.Fade(rlc.WHITE, 0.6) if s % 2 else rlc.Fade(rlc.GREEN, 0.3)
+            rlc.DrawCircleSector(
+                center(),
+                outer_radius,
+                180 + seg_angle * s,
+                180 + seg_angle * s + seg_angle,
+                segments,
+                color,
+            )
+            rlc.DrawCircleSectorLines(
+                center(),
+                outer_radius,
+                180 + seg_angle * s,
+                180 + seg_angle * s + seg_angle,
+                segments,
+                color,
+            )
+
+        rlc.DrawCircleSector(
+            center(),
+            outer_radius,
+            180 + self.start_target,
+            180 + self.end_target,
+            segments,
+            rlc.Fade(rlc.YELLOW, 0.9),
+        )
+
+        rlc.DrawCircleSector(
+            center(),
+            outer_radius,
+            180 + self.curr_angle,
+            180 + self.curr_angle + 1,
+            segments,
+            rlc.Fade(rlc.MAROON, 0.5),
+        )
+
+
 class Player:
     def __init__(self, pos):
         self.pos = pos
         self.camera = rl.Camera2D()
         self.camera.offset = (Vec2(WINDOW_WIDTH, WINDOW_HEIGHT) / 2)()
         self.camera.zoom = 10
-        self.state = MOVING
-        self.fishing_timer = 0
-        self.fish_lure_pos = Vec2(0, 0)
+        self.state = State.MOVING
 
         # inventory
-        self.tool = AXE
+        self.tool = Tool.AXE
 
         self.wood = 0
         self.sappling = 0
-        self.fish = 0
 
+        self.fishing = FishingManager()
+
+        # stats
         self.MAX_FOOD = 10
         self.food_timer = 0
         self.food = 10
@@ -139,29 +243,19 @@ class Player:
         self.wetness_timer = 0
         self.wetness = 0
 
-    def move(self):
+    def move(self, world):
         dx = rlc.IsKeyDown(rlc.KEY_D) - rlc.IsKeyDown(rlc.KEY_A)
         dy = rlc.IsKeyDown(rlc.KEY_S) - rlc.IsKeyDown(rlc.KEY_W)
-        if dir := Vec2(dx, dy):
-            self.pos += dir / dir.len() * rlc.GetFrameTime() * 100
-
-    def _fish(self, world):
-        self.fishing_timer += rlc.GetFrameTime()
-        if self.fishing_timer >= 3:
-            self.fishing_timer = 0
-            self.state = MOVING
-            print("from _fish", self.fish_lure_pos)
-            print(world.blocks[int(self.fish_lure_pos.y)][int(self.fish_lure_pos.x)])
-            world.blocks[int(self.fish_lure_pos.y)][int(self.fish_lure_pos.x)] = WATER
-            print(world.blocks[int(self.fish_lure_pos.y)][int(self.fish_lure_pos.x)])
-            self.fish_lure_pos = Vec2(0, 0)
+        self.dir = Vec2(dx, dy)
+        if self.dir:
+            self.pos += self.dir / self.dir.len() * rlc.GetFrameTime() * 100
+            start_pos = int((world.size.x - world.island_size) / 2) * BSIZE
+            end_pos = int(start_pos + world.island_size * BSIZE)
+            self.pos.x = max(start_pos, min(self.pos.x, end_pos))
+            self.pos.y = max(start_pos, min(self.pos.y, end_pos))
 
     def update(self, world):
-        # update food
-        if rlc.IsKeyPressed(rlc.KEY_U):
-            self.food -= 1
-            self.food = min(10, max(0, self.food))
-
+        # debugg update food
         self.food_timer += rlc.GetFrameTime()
         while self.food_timer >= 10:
             self.food_timer -= 10
@@ -169,63 +263,66 @@ class Player:
             self.food = min(10, max(0, self.food))
 
         # update wetness
-        if rlc.IsKeyPressed(rlc.KEY_U):
-            self.wetness += 1
-            self.wetness = min(10, max(0, self.wetness))
-
         self.wetness_timer += rlc.GetFrameTime()
         while self.wetness_timer >= 10:
             self.wetness_timer -= 10
             self.wetness += 1
             self.wetness = min(10, max(0, self.wetness))
 
-        if self.state == MOVING:
-            self.move()
-
-        if self.state == FISHING:
-            self._fish(world)
+        match self.state:
+            case State.MOVING:
+                self.move(world)
+            case State.FISHING:
+                self.fishing.update(self, world)
 
         self.camera.target = self.pos()
         self.camera.zoom = math.exp(
             math.log(self.camera.zoom) + rlc.GetMouseWheelMove() * 0.1
         )
 
+        # interract
         # mouse block coordinates
         mpos = rlc.GetScreenToWorld2D(rlc.GetMousePosition(), self.camera)
         W_x = int(max(0, min(world.size.x - 1, mpos.x / BSIZE)))
         W_y = int(max(0, min(world.size.y - 1, mpos.y / BSIZE)))
 
         if rlc.IsKeyPressed(rlc.KEY_TWO):
-            self.tool = FISHING_ROD
+            self.tool = Tool.FISHING_ROD
         elif rlc.IsKeyPressed(rlc.KEY_ONE):
-            self.tool = AXE
+            self.tool = Tool.AXE
 
-        if rlc.IsMouseButtonPressed(rlc.MOUSE_BUTTON_LEFT) and self.state == MOVING:
+        if (
+            rlc.IsMouseButtonPressed(rlc.MOUSE_BUTTON_LEFT)
+            and self.state == State.MOVING
+        ):
             # break tree
-            if world.blocks[W_y][W_x] == TREE and self.tool == AXE:
+            if world.blocks[W_y][W_x] == Block.TREE and self.tool == Tool.AXE:
                 dx = (mpos.x - self.pos.x) ** 2
                 dy = (mpos.y - self.pos.y) ** 2
                 if math.sqrt(dx + dy) < 15:
-                    world.blocks[W_y][W_x] = SAND
+                    world.blocks[W_y][W_x] = Block.SAND
                     self.wood += 1
                     self.sappling += 1 if random.uniform(0, 1) <= 0.33 else 0
                 # else:
                 #     return "You are too far from the tree!"
             # fishing
-            elif world.blocks[W_y][W_x] == WATER and self.tool == FISHING_ROD:
-                self.state = FISHING
-                world.blocks[W_y][W_x] = FISH_LURE
-                self.fish_lure_pos = Vec2(W_x, W_y)
-                print("set fish lure at", self.fish_lure_pos, "or", Vec2(W_y, W_x))
-                print(world.blocks[W_y][W_x])
-        elif rlc.IsMouseButtonPressed(rlc.MOUSE_BUTTON_LEFT) and self.state == FISHING:
-            # fish behaviour
-            pass
+            elif (
+                world.blocks[W_y][W_x] == Block.WATER and self.tool == Tool.FISHING_ROD
+            ):
+                self.state = State.FISHING
+                world.blocks[W_y][W_x] = Block.FISH_LURE
+                self.fishing.lure_pos = Vec2(W_x, W_y)
+                self.fishing.set_target()
+        elif (
+            rlc.IsMouseButtonPressed(rlc.MOUSE_BUTTON_LEFT)
+            and self.state == State.FISHING
+        ):
+            self.fishing.pull_fish()
 
         if rlc.IsMouseButtonPressed(rlc.MOUSE_BUTTON_RIGHT):
-            if world.blocks[W_y][W_x] == SAND and self.wood >= 50:
+            if world.blocks[W_y][W_x] == Block.SAND and self.wood >= 50:
                 self.wood -= 50
-                world.blocks[W_y][W_x] = FIRE
+                world.blocks[W_y][W_x] = Block.FIRE
             # else:
             #     return "You need more wood to build FIRE!"
 
@@ -233,10 +330,12 @@ class Player:
         # body
         rlc.DrawCircleV(self.pos(), 2, rlc.RED)
         # head
-        rlc.DrawCircleV((self.pos - Vec2(0, 2))(), 1, rl.Color(224, 183, 94, 255))
+        rlc.DrawCircleV(
+            (self.pos - Vec2(0, 2) + self.dir * 0.4)(), 1, rl.Color(224, 183, 94, 255)
+        )
 
         # equipped tool
-        if self.tool == AXE:
+        if self.tool == Tool.AXE:
             rlc.DrawRectangleV(
                 Vec2(self.pos.x + 3, self.pos.y - 2)(), Vec2(0.5, 4)(), rlc.DARKBROWN
             )
@@ -246,7 +345,7 @@ class Player:
                 Vec2(self.pos.x + 5, self.pos.y)(),
                 rlc.GRAY,
             )
-        elif self.tool == FISHING_ROD:
+        elif self.tool == Tool.FISHING_ROD:
             rlc.DrawRectangleV(
                 Vec2(self.pos.x + 3, self.pos.y - 8)(), Vec2(0.5, 10)(), rlc.DARKBROWN
             )
@@ -254,7 +353,7 @@ class Player:
 
             # fishing line
             rlc.DrawCircleV(fish_tip(), 0.5, rlc.BROWN)
-            if self.state == FISHING:
+            if self.state == State.FISHING:
                 line_points = 20
                 line_coords = []
                 for i in range(line_points):
@@ -262,19 +361,20 @@ class Player:
                     line_coords.append(
                         rlc.GetSplinePointLinear(
                             fish_tip(),
-                            (self.fish_lure_pos * BSIZE + Vec2(BSIZE / 2, BSIZE / 2))(),
+                            (
+                                self.fishing.lure_pos * BSIZE
+                                + Vec2(BSIZE / 2, BSIZE / 2)
+                            )(),
                             T,
                         )
                     )
-                    # if i not in (0, line_points - 1):
-                    #     line_coords[-1].y -= 30 * ((T - 0.5) ** 2)
                     line_coords[-1].y -= 60 * ((T - 0.5) ** 2) - 15
                 rlc.DrawSplineLinear(line_coords, line_points, 0.5, rlc.BLACK)
 
     def draw_stats(self):
         # items quantity
         text = (
-            f"Wood: {self.wood} | Sappling: {self.sappling} | Fish: {self.fish}"
+            f"Wood: {self.wood} | Sappling: {self.sappling} | Fish: {self.fishing.fish}"
         ).encode()
         rlc.DrawRectangleV(
             Vec2(0, 0)(), Vec2(30 * len(text), 70)(), rl.Color(0, 0, 0, 100)
@@ -282,9 +382,7 @@ class Player:
         rlc.DrawText(text, 10, 10, 50, rlc.WHITE)
 
         # equiped tool tooltip
-        text = (
-            "Equiped Tool: " + ("AXE" if self.tool == AXE else "FISHING_ROD")
-        ).encode()
+        text = ("Equiped Tool: " + self.tool.name).encode()
         rlc.DrawRectangleV(
             Vec2(0, 70)(), Vec2(30 * len(text), 70)(), rl.Color(0, 0, 0, 100)
         )
@@ -318,19 +416,25 @@ class Player:
 
 def main():
     rlc.InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, b"First")
-    player = Player(Vec2(10, 10))
     world = World()
     world.generate_island()
     world.spawn_trees()
+    # player = Player(Vec2(world.size.x // 2 * BSIZE, world.size.y // 2 * BSIZE))
+    player = Player(Vec2(130, 130))
     while not rlc.WindowShouldClose():
         player.update(world)
         rlc.BeginDrawing()
+
         rlc.BeginMode2D(player.camera)
         rlc.ClearBackground(rlc.BLACK)
         world.draw()
         player.draw()
         rlc.EndMode2D()
+
         player.draw_stats()
+        if player.state == State.FISHING:
+            player.fishing.draw()
+
         rlc.EndDrawing()
     rlc.CloseWindow()
 
